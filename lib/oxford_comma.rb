8f3b50cb008173ee0c2c141dd5d433015c05fa75
ERIC KELLY <eakelly220@gmail.com>
2018-04-15 19:37:54 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,5 @@
 def oxford_comma(array)
+<<<<<<< HEAD
   if array.length == 1 
    return array.join(", ")
   elsif array.length == 2 
@@ -10,4 +11,9 @@ def oxford_comma(array)
     newArray << last
     return newArray
     end 
+=======
+last = array.pop
+array.join(", ")
+
+>>>>>>> 0900cc5160d996949eebc253efc6a6b42b0fbe89
 end
